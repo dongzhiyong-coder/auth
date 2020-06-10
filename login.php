21540d06e8e1ec01cda3888b4f86bba6e1f7f3b0
dongzhiyong <dongzy@xinruiying.com>
2020-06-10 15:05:23 +0800

    登录功能

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm
+ * Author Zhiyong Dong <dongzy@xinruiying.com>
+ * Date:2020/6/10
+ * Time:13:18
+ */
+
+//设置错误级别
+error_reporting(0);
+//加载公共函数
+require __DIR__."/lib/function.php";
+//加载pdo操作类
+require __DIR__."/lib/mysql.php";
+//加载数据库配置文件
+$db_config = require __DIR__."/config/db_config.php";
+$mysql = new mysql($db_config);
+$user_name = $_POST['user_name'];
+$user_password = $_POST['user_password'];
+$user = $mysql->table('pc_user')->where("user_name='".$user_name."'")->find();
+if(empty($user)){
+    show_json('500','用户不存在');
+}
+if(md5($_POST['user_password'].'djhh#@22**hh777&')!=$user['user_password']){
+    show_json('500','密码错误');
+}
+show_json('200','登录成功');
+
+
+
+
+
