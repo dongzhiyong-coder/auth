d7c40f33c45b795321cb23801836019791aa1d56
dongzhiyong <dongzy@xinruiying.com>
2020-06-10 14:16:46 +0800

    新增用户新增方法

--- addUser.php
+++ addUser.php
@@ -6,8 +6,33 @@
  * Time:13:18
  */
 
-require "./lib/mysql.php";
-$db_config = require "./config/db_config.php";
+//设置错误级别
+error_reporting(0);
+//加载公共函数
+require __DIR__."/lib/function.php";
+//加载pdo操作类
+require __DIR__."/lib/mysql.php";
+//加载数据库配置文件
+$db_config = require __DIR__."/config/db_config.php";
 $mysql = new mysql($db_config);
-$res = $mysql->table('pc_user')->where("user_id=1")->select();
-print_r($res);
+$_POST['user_name'] = 'admin';
+$_POST['user_password'] = '123456';
+$data['user_name'] = $_POST['user_name'];
+$data['user_password'] = md5($_POST['user_password'].'djhh#@22**hh777&');
+$data['create_time'] = date('Y-m-d H:i:s');
+try {
+    $res = $mysql->table('pc_user')->insert($data);
+    if($res>0){
+        show_json('','ok');
+    }
+    else{
+        show_json(500,'系统错误');
+    }
+}
+catch (\Exception $e){
+        show_json(500,$e->getMessage());
+}
+
+
+
+
--- lib/function.php
+++ lib/function.php
@@ -10,3 +10,11 @@ function p($data){
     echo json_encode($data);
     die;
 }
+
+function show_json($code=200,$msg='',$data=[]){
+    $arr['code'] = $code;
+    $arr['msg'] = $msg;
+    $arr['data'] = $data;
+    echo json_encode($arr);
+    die;
+}
